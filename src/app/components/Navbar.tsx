4534802b8ec43d29a25670d1f439edd6d4e3a454
Christian Carbonari <christiancarbonari9@gmail.com>
2025-05-22 18:57:40 +1000

    navbar and above-the-fold draft created

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,16 +1,40 @@
 import Link from 'next/link'
+import Image from 'next/image';
 
 const Navbar = () => {
 
-  const navLinks = [
-    { label: 'Home', path: '/' },
-    { label: 'Projects', path: '/projects' },
-    { label: 'About', path: '/about' },
-    { label: 'Contact', path: '/contact' },
-  ]
-
   return (
-    <nav className="flex items-center justify-between p-4 shadow-md bg-white">
+    <nav className="flex items-center justify-between p-4 shadow-md bg-white h-fit">
+      <Image
+        src="/logo.png"
+        alt="personal logo"
+        height={50}
+        width={150}
+      />
+
+      <div className="links">
+        <Link href="/" className="text-gray-800 hover:text-gray-900">
+          Home
+        </Link>
+        <Link href="#" className="ml-4 text-gray-800 hover:text-gray-900">
+          Linkedin
+        </Link>
+        <Link href="#" className="ml-4 text-gray-800 hover:text-gray-900">
+          X.com
+        </Link>
+      </div>
+
+      <div className="flex items-center">
+        <Image
+          src="/envelope-solid.svg"
+          alt="envelope email icon"
+          height={20}
+          width={20}
+        />
+        <p className="ml-4 text-gray-800">
+          christiancarbonari9@gmail.com
+        </p>
+      </div>
 
     </nav>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,30 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>Hello World!</h1>
+    <div>
+      <div className="bg-[url('/above-the-fold.jpg')]  h-screen flex">
+        <div className="w-1/2">
+          <div>
+            <h1>
+              Hi, I'm Christian.<br/>
+              I'm a Freelance Shopify Developer
+            </h1>
+          </div>
+        </div>
+        <div className="w-1/2">
+          <Image
+            className="rounded-xl"
+            src="/profile-picture-blue-bg.jpg"
+            alt="profile picture"
+            width={300}
+            height={300}
+          />
+        </div>
+      </div>
+
+      <div className="bg-[url('/below-the-fold.jpg')] bg-cover h-[1000px]">
+
+      </div>
+    </div>
   );
 }
